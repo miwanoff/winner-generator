35fdf0e46ec79d0cd18143d85491665fe21c107b
777dan <danya.nikitin7@gmail.com>
2022-12-26 17:53:58 +0200

    resolve students.json

--- admin-panel.php
+++ admin-panel.php
@@ -1,2 +1,12 @@
 <?php
 echo "<h1>Winner generator</h1>";
+echo "<form action=" . $_SERVER['PHP_SELF'] . " method='post'>
+Введіть ім'я: <input type='text' name='name'><br>
+Введіть прізвище: <input type='text' name='surname'><br>
+Введіть посилання на Інстаграм: <input type='text' name='link_instagram'><br>
+Введіть посилання на роботу в Figma: <input type='text' name='link_figma'><br>
+<input type='submit' name='submit' value='Підтвердити'><br>
+</form>";
+// if (isset($_POST['submit'])) {
+//     echo $_POST['name'];
+// }

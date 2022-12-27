e762c4b43fa9a9b0aa693c8b5fa14afdbe3a63f1
knika-starkey <knika787@gmail.com>
2022-12-27 22:45:42 +0200

    a bit changes of info in table

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ fetch("students.json")
     showPart(data);
   });
 function showPart(students) {
-  let str = `<table><tr><th>Name</th><th>Surname</th><th>Instagram link</th><th>Figma link</th><th>Nickname</th></tr>`;
+  let str = `<table><tr><th>Name and Surname</th><th>Instagram link</th><th>Figma link</th></tr>`;
   for (let i = 0; i < students.length; i++) {
-    str += `<tr><td>${students[i].name}</td><td>${students[i].surname}</td><td>${students[i].link_instagram}</td><td>${students[i].link_figma}</td><td>${students[i].nickname}</td></tr>`;
+    str += `<tr><td>${students[i].student}</td><td>${students[i].link_instagram}</td><td>${students[i].link_figma}</td></tr>`;
   }
   str += `</table>`;
   partBlock.innerHTML = str;

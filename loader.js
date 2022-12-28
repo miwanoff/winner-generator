725f3c7b9f1b1a61be8cfbe9f593019930fe5490
miwanoff <marina.iva.noffa@gmail.com>
2022-12-28 15:43:19 +0200

    Add common functionality

--- loader.js
+++ loader.js
@@ -1,12 +1,15 @@
 let partBlock = document.querySelector("#participants");
+let students =[];
 fetch("students.json")
   .then((response) => {
     return response.json();
   })
   .then((data) => {
+    students = data;
     showPart(data);
   });
 function showPart(students) {
+   
   let str = `<table class="table table-striped table-hover"><thead><tr><th>Name</th><th>Surname</th><th>Instagram link</th><th>Figma link</th><th>Nickname</th></tr></thead><tbody>`;
   for (let i = 0; i < students.length; i++) {
     str += `<tr><td>${students[i].name}</td><td>${students[i].surname}</td><td>${students[i].link_instagram}</td><td>${students[i].link_figma}</td><td>${students[i].nickname}</td></tr>`;

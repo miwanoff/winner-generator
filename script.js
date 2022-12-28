ee79ea92e9047c18651c1ac46b6b175eec7a67df
AlexHabibi <90087414+AlexHabibi@users.noreply.github.com>
2022-12-28 16:42:17 +0100

    3

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-const students = [   
-  {
-    name: "имя 1",
-    surname: "фамилия 1",
-    link_instagram: "ссылка на аккаунт в инстаграме 1",
-    link_figma: "ссылка на работу в Figma 1",
-    nickname: "ник в инстаграме 1",
-  },
-  {
-    name: "имя 2",
-    surname: "фамилия 2",
-    link_instagram: "ссылка на аккаунт в инстаграме 2",
-    link_figma: "ссылка на работу в Figma 2",
-    nickname: "ник в инстаграме 2",
-  },
-  {
-    name: "имя 3",
-    surname: "фамилия 3",
-    link_instagram: "ссылка на аккаунт в инстаграме 3",
-    link_figma: "ссылка на работу в Figma 3",
-    nickname: "ник в инстаграме 3",
-  },
-  {
-    name: "имя 4",
-    surname: "фамилия 4",
-    link_instagram: "ссылка на аккаунт в инстаграме 4",
-    link_figma: "ссылка на работу в Figma 4",
-    nickname: "ник в инстаграме 4",
-  },
+// const students = [   
+//   {
+//     name: "имя 1",
+//     surname: "фамилия 1",
+//     link_instagram: "ссылка на аккаунт в инстаграме 1",
+//     link_figma: "ссылка на работу в Figma 1",
+//     nickname: "ник в инстаграме 1",
+//   },
+//   {
+//     name: "имя 2",
+//     surname: "фамилия 2",
+//     link_instagram: "ссылка на аккаунт в инстаграме 2",
+//     link_figma: "ссылка на работу в Figma 2",
+//     nickname: "ник в инстаграме 2",
+//   },
+//   {
+//     name: "имя 3",
+//     surname: "фамилия 3",
+//     link_instagram: "ссылка на аккаунт в инстаграме 3",
+//     link_figma: "ссылка на работу в Figma 3",
+//     nickname: "ник в инстаграме 3",
+//   },
+//   {
+//     name: "имя 4",
+//     surname: "фамилия 4",
+//     link_instagram: "ссылка на аккаунт в инстаграме 4",
+//     link_figma: "ссылка на работу в Figma 4",
+//     nickname: "ник в инстаграме 4",
+//   },
 
-];
+// ];
 
 
 function func() {

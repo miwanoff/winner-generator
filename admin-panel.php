ba7691f46c637c19380308912b2bb13d1b454644
miwanoff <marina.iva.noffa@gmail.com>
2022-12-28 15:34:05 +0200

    Add functionality

--- admin-panel.php
+++ admin-panel.php
@@ -1,50 +1,106 @@
-<?php
-echo "<h1>Winner generator</h1>";
-echo "<form action=" . $_SERVER['PHP_SELF'] . " method='post'>
-Введіть ім'я: <input type='text' name='name'><br>
-Введіть прізвище: <input type='text' name='surname'><br>
-Введіть посилання на Інстаграм: <input type='text' name='link_instagram'><br>
-Введіть посилання на роботу в Figma: <input type='text' name='link_figma'><br>
-Введіть нік в Інстаграмі: <input type='text' name='nickname'><br>
-<input type='submit' name='submit' value='Підтвердити'><br>
-<input type='submit' name='winner' value='Знайти переможця'><br>
-</form>";
-$json_file = fopen("students.json", "a+");
-$content_json_file = fread($json_file, filesize("students.json"));
-$students = json_decode($content_json_file);
-$name = $_POST['name'];
-$surname = $_POST['surname'];
-$link_instagram = $_POST['link_instagram'];
-$link_figma = $_POST['link_figma'];
-$nickname = $_POST['nickname'];
-$lengthArr = count($students);
-// print_r($students);
-if (isset($_POST['submit'])) {
-    $studentsNewArr = $students;
-    $studentsNewArr[$lengthArr] = [
-        'name' => $name,
-        'surname' => $surname,
-        'link_instagram' => $link_instagram,
-        'link_figma' => $link_figma,
-        'nickname' => $nickname
-    ];
-    $studentsNewArr = json_encode($studentsNewArr, JSON_UNESCAPED_UNICODE);
-    $json_fileClear = fopen("students.json", "w+");
-    fwrite($json_fileClear, $studentsNewArr);
-    print_r($studentsNewArr);
-    fclose($json_fileClear);
-}
-if (isset($_POST['winner'])) {
-    $randNum = rand(0, $lengthArr - 1);
-    foreach ($students as $key => $value) {
-        if ($value->name == $name && $value->surname == $surname && $value->link_instagram == $link_instagram && $value->link_figma == $link_figma && $value->nickname == $nickname) {
-            if ($key == $randNum) {
-                echo "Переможець: $value->name";
-                break;
-            }
-        } else {
-            echo "У базі даних немає такого учасника!";
-            break;
+<?php ob_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <style>
+        h3 {
+            color: green;
+            margin-left: 20px;
         }
+
+        input {
+            width: 300px !important;
+        }
+
+        form {
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid p-4 bg-primary text-white text-center">
+        <h1>Winner generator</h1>
+    </div>
+    <div class="container-fluid p-4">
+    <form action=<?= $_SERVER['PHP_SELF'] ?> method='post'>
+        <div class=" mb-3 mt-3">
+        Введіть ім'я: <input type='text' class="form-control" name='name'>
+    </div>
+    <div class="mb-3">
+        Введіть прізвище: <input type='text' class="form-control" name='surname'>
+    </div>
+    <div class="mb-3">
+        Введіть посилання на Інстаграм: <input type='text' class="form-control" name='link_instagram'>
+    </div>
+    <div class="mb-3">
+        Введіть посилання на роботу в Figma: <input type='text' class="form-control" name='link_figma'>
+    </div>
+    <div class="mb-3">
+        Введіть нік в Інстаграмі: <input type='text' class="form-control" name='nickname'>
+    </div>
+    <input type='submit' name='submit' class="btn btn-primary" value='Підтвердити'>
+    <!-- <input type='submit' name='winner' class="btn btn-primary" value='Знайти переможця'><br> -->
+    </form>
+    <form action=<?= $_SERVER['PHP_SELF'] ?> method='post'>
+        <input type='submit' name='clear' class="btn btn-primary" value='Очистити базу даних'>
+    </form>
+    <form action=<?= $_SERVER['PHP_SELF'] ?> method='post'>
+        <input type='submit' name='main' class="btn btn-primary" value='Перейти до головної сторінки'>
+    </form>
+    </div>
+    <?php
+    $json_file = fopen("students.json", "a+");
+    $content_json_file = fread($json_file, filesize("students.json"));
+    $students = json_decode($content_json_file);
+    // print_r($students);
+
+    //занесення учасників до бази даних
+    if (isset($_POST['submit'])) {
+        $studentsNewArr = $students;
+        $studentsNewArr[count($students)] = [
+            'name' => $_POST['name'],
+            'surname' => $_POST['surname'],
+            'link_instagram' => $_POST['link_instagram'],
+            'link_figma' => $_POST['link_figma'],
+            'nickname' => $_POST['nickname']
+        ];
+        $studentsNewArr = json_encode($studentsNewArr, JSON_UNESCAPED_UNICODE);
+        $json_fileClear = fopen("students.json", "w+");
+        fwrite($json_fileClear, $studentsNewArr);
+        // print_r($studentsNewArr);
+        fclose($json_fileClear);
+        echo "<h3>Дані занесено успішно</h3>";
     }
-}
+
+    // //визначення переможця
+    // if (isset($_POST['winner'])) {
+    //     $randNum = rand(0, count($students) - 1);
+    //     foreach ($students as $key => $value) {
+    //         echo "Переможець: $value->name";
+    //         break;
+    //     }
+    // }
+
+    //очищення бази даних 
+    if (isset($_POST['clear'])) {
+        $clearJSONfile = fopen("students.json", "w+");
+        fwrite($clearJSONfile, "[]");
+        fclose($clearJSONfile);
+    }
+
+    //переадресація користувача на головну сторінку
+    if (isset($_POST['main'])) {
+        header("Location: index.php");
+        ob_end_flush();
+    }
+    ?>
+</body>
+
+</html>
--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-let partBlock = document.querySelector("#participants");
-fetch("students.json")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    showPart(data);
-  });
-function showPart(students) {
-  let str = `<table><tr><th>Name</th><th>Surname</th><th>Instagram link</th><th>Figma link</th><th>Nickname</th></tr>`;
-  for (let i = 0; i < students.length; i++) {
-    str += `<tr><td>${students[i].name}</td><td>${students[i].surname}</td><td>${students[i].link_instagram}</td><td>${students[i].link_figma}</td><td>${students[i].nickname}</td></tr>`;
+const students = {
+    participants: [
+      {
+        name: "имя 1",
+        surname: "фамилия 1",
+        link_instagram: "ссылка на аккаунт в инстаграме 1",
+        link_figma: "ссылка на работу в Figma 1",
+        nickname: "ник в инстаграме 1",
+      },
+      {
+        name: "имя 2",
+        surname: "фамилия 2",
+        link_instagram: "ссылка на аккаунт в инстаграме 2",
+        link_figma: "ссылка на работу в Figma 2",
+        nickname: "ник в инстаграме 2",
+      },
+      {
+        name: "имя 3",
+        surname: "фамилия 3",
+        link_instagram: "ссылка на аккаунт в инстаграме 3",
+        link_figma: "ссылка на работу в Figma 3",
+        nickname: "ник в инстаграме 3",
+      },
+      {
+        name: "имя 4",
+        surname: "фамилия 4",
+        link_instagram: "ссылка на аккаунт в инстаграме 4",
+        link_figma: "ссылка на работу в Figma 4",
+        nickname: "ник в инстаграме 4",
+      },
+    ],
+  };
+  const rand = Math.floor(Math.random() * students.participants.length);
+  let result = "";
+    function func(){ 
+  for (const key in students.participants[rand]) {
+    if (Object.hasOwnProperty.call(students.participants[rand], key)) {
+      result += `<p>key: ${students.participants[rand][key]}\n</p>`;
+    }
   }
-  str += `</table>`;
-  partBlock.innerHTML = str;
-}
+  document.getElementById("para").innerHTML = result; 
+  };

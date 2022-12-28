f06f422d0f4d9a3a4a32cc08da9ad83905fa0561
AlexHabibi <90087414+AlexHabibi@users.noreply.github.com>
2022-12-28 16:47:26 +0100

    5

--- script.js
+++ script.js
@@ -31,25 +31,27 @@
 // ];
 
 
+
 function func() {
-let result = `<div class="container mt-3">`;
-result += `
-<div class="alert alert-primary">
-<h2>Winner</h2>
-</div>
+  let result = `<div class="container mt-3">`;
+  result += `
+  <div class="alert alert-primary">
+  <h2 class = "text-center">Winner</h2>
+  </div>
 </div>`;
-result += `<div class="row"><div class="col-sm-4 p-3 bg-primary text-white m-auto">`;
-const rand = Math.floor(Math.random() * students.length);
-for (const key in students[rand]) {
-    if (Object.hasOwnProperty.call(students[rand], key)) {
-        if (key == "link_instagram" || key == "link_figma") {
-            result += `<p>${key}: <strong><a href="${students[rand][key]}" class="text-white" target="_blank">${students[rand][key]}</a></strong>\n</p>`;
-        } else {
-            result += `<p>${key}: <strong>${students[rand][key]}</strong>\n</p>`;
-        }
-    }
-}
-document.getElementById("para").innerHTML = `${result}</div></div></div>`;
+  result += `<div class="row"><div class="col-sm-4 p-3 bg-primary text-white m-auto">`;
+  const rand = Math.floor(Math.random() * students.length);
+  for (const key in students[rand]) {
+      if (Object.hasOwnProperty.call(students[rand], key)) {
+          if (key == "link_instagram" || key == "link_figma") {
+              result += `<p>${key}: <strong><a href="${students[rand][key]}" class="text-white" target="_blank">${students[rand][key]}</a></strong>\n</p>`;
+          } else {
+              result += `<p>${key}: <strong>${students[rand][key]}</strong>\n</p>`;
+          }
+      }
+  }
+  document.getElementById("para").innerHTML = `${result}</div></div></div>`;
 }
 const buttonGenerate = document.getElementById("generate");
 buttonGenerate.addEventListener("click", func); 
+

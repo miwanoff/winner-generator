2327308eabca7994e74192cac3fe17c6abb620b9
777dan <danya.nikitin7@gmail.com>
2022-12-28 13:12:40 +0200

    fixed some problems opportunity to clear database

--- admin-panel.php
+++ admin-panel.php
@@ -7,23 +7,17 @@ echo "<form action=" . $_SERVER['PHP_SELF'] . " method='post'>
 Введіть посилання на роботу в Figma: <input type='text' name='link_figma'><br>
 Введіть нік в Інстаграмі: <input type='text' name='nickname'><br>
 <input type='submit' name='submit' value='Підтвердити'><br>
-<input type='submit' name='winner' value='Знайти переможця'><br><br>
-Введіть назву для JSON-файлу без «.json»: <input type='text' name='fileJson'><br>
-<input type='submit' name='submitFile' value='Створити'><br>
-</form><br>";
-// $students = [];
-// $filename = "ffht";
-if (isset($_POST['submitFile'])) {
-    $filename = $_POST['fileJson'];
-    $json_file = fopen("$filename.json", "a+");
-    fwrite($json_file, "[]");
-    $content_json_file = fread($json_file, filesize("$filename.json"));
-    $students = json_decode($content_json_file);
-}
+<input type='submit' name='winner' value='Знайти переможця'><br>
+</form>";
+echo "<form action=" . $_SERVER['PHP_SELF'] . " method='post'>
+<input type='submit' name='clear' value='Очистити базу даних'><br>
+</form>";
+$json_file = fopen("students.json", "a+");
+$content_json_file = fread($json_file, filesize("students.json"));
+$students = json_decode($content_json_file);
+// print_r($students);
 
-// if (filesize("$filename.json") == 0) {
-//     fwrite($json_file, "[]");
-// }
+//занесення учасників до бази даних
 if (isset($_POST['submit'])) {
     $studentsNewArr = $students;
     $studentsNewArr[count($students)] = [
@@ -34,22 +28,24 @@ if (isset($_POST['submit'])) {
         'nickname' => $_POST['nickname']
     ];
     $studentsNewArr = json_encode($studentsNewArr, JSON_UNESCAPED_UNICODE);
-    $json_fileClear = fopen("$filename.json", "w+");
+    $json_fileClear = fopen("students.json", "w+");
     fwrite($json_fileClear, $studentsNewArr);
     // print_r($studentsNewArr);
     fclose($json_fileClear);
 }
+
+//визначення переможця
 if (isset($_POST['winner'])) {
     $randNum = rand(0, count($students) - 1);
     foreach ($students as $key => $value) {
-        if ($value->name == $_POST['name'] && $value->surname == $_POST['surname'] && $value->link_instagram == $_POST['link_instagram'] && $value->link_figma == $_POST['link_figma'] && $value->nickname == $_POST['nickname']) {
-            if ($key == $randNum) {
-                echo "Переможець: $value->name";
-                break;
-            }
-        } else {
-            echo "У базі даних немає такого учасника!";
-            break;
-        }
+        echo "Переможець: $value->name";
+        break;
     }
+}
+
+//очищення бази даних 
+if (isset($_POST['clear'])) {
+    $clearJSONfile = fopen("students.json", "w+");
+    fwrite($clearJSONfile, "[]");
+    fclose($clearJSONfile);
 }

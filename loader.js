eba5a49233f108498d36fa85b35393d02ee8e143
knika-starkey <knika787@gmail.com>
2022-12-28 12:54:22 +0200

    table style with bootstrap

--- loader.js
+++ loader.js
@@ -7,10 +7,10 @@ fetch("students.json")
     showPart(data);
   });
 function showPart(students) {
-  let str = `<table><tr><th>Name</th><th>Surname</th><th>Instagram link</th><th>Figma link</th><th>Nickname</th></tr>`;
+  let str = `<table class="table table-striped table-hover"><thead><tr><th>Name</th><th>Surname</th><th>Instagram link</th><th>Figma link</th><th>Nickname</th></tr></thead><tbody>`;
   for (let i = 0; i < students.length; i++) {
     str += `<tr><td>${students[i].name}</td><td>${students[i].surname}</td><td>${students[i].link_instagram}</td><td>${students[i].link_figma}</td><td>${students[i].nickname}</td></tr>`;
   }
-  str += `</table>`;
+  str += `</tbody></table>`;
   partBlock.innerHTML = str;
 }
